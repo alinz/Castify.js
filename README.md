e645a4a6c46e62f52471586871e01c2f67d76e06
Ali Najafizadeh <a.najafizadeh@gmail.com>
2014-10-28 22:47:24 -0400

    first version released

---
+++ README.md
@@ -0,0 +1,190 @@
+# Castify.js
+
+## Introduction
+
+I know you don't have much time to read this doc, so do I (just kidding) :D, so I made it dead simple. If you answer `YES` to at least one of the following questions, seriously, you should continue reading this doc.
+
+1. Do you have a ChromeCast device and you would like to talk to it with your browser?
+2. You read the google dev and provided examples are just too confusing... because you just want to do a simple thing only
+3. You just want to play a video or music but ...
+
+.
+.
+.
+.
+
+Ahaaa! You are still here and reading. So I guess you are interested in what I am going to say in this doc. Please continue reading...
+
+`Castify` is a wrapper around `Google ChromeCast sender api` which makes it a lot easier to work with ChromeCast's apis. I'm planning to add more features as time goes by, but at the moment the following things are supported.
+
+1. Supports playback video and music
+2. Supports cover image and title for both video and music
+3. Supports realtime update about every state of Chorme Cast
+	* Session is started
+	* Session is terminated
+	* Session is recovered
+	* many more
+4. Supports play, stop, pause and seek feature for media player
+5. No more multiple callbacks.
+6. Proper error handling which dead simple! :D
+
+## Usage
+
+Enough impressing you, let's see some actions.
+
+First, add the following script tags right after google's api sender script.
+
+```js
+<script src="https://www.gstatic.com/cv/js/sender/v1/cast_sender.js"></script>
+<script src="castify.js"><script>
+```
+
+Second, use `Castify.ready()` function to register your main function.
+
+```js
+function myMain() {
+	...
+}
+
+Castify.ready(function (loaded, err) {
+	if (loaded) {
+		myMain();
+	} else {
+		console.log(err);
+	}	
+});
+``` 
+
+`Castify.ready` makes sure that everything is ready before calling your main function.
+
+Third, Create a session, so we can talk to Chrome Cast device. **AND THAT'S IT!**
+
+```js
+function myMain() {
+	var session = Castify.createSession({
+		appId: "your app id", //optional
+		policy: Castify.POLICY_SAME_URL //optional
+	});
+	session.status = function (status) {
+		...
+	};
+}
+```
+
+So in order to create a session, we need to create one. `Castify.createSession` is a helper function which creates a session and binds all sort of callbacks.
+
+`createSession` function accept one argument, `options`. `options` has 2 fields, `appId` and `policy`.
+
+`appId` is a string which you get from Google Chrome Cast once you are registering with them. **However**, if you simply want to stream something to your ChromeCast device, you don't need to have one. If you omit this field, `Castify` will use the default app id.
+
+> **Note** You can check the Google Dev site to see the difference between default and registered app ids.
+
+ 
+`policy` is also an optional field. `policy` is defined by ChormeCast api which has `3` possibilities which I wrapped them with `Castify` static variables.
+
+> **Note** The definition of each policies are copied from Google Doc directly.
+
+1. Castify.POLICY_RESTRICT
+> No automatic connection.
+	
+2. Castify.POLICY_SAME_URL
+> Automatically connects when the session was started with the same appId and the same page origin (regardless of tab).
+    
+3. Castify.POLICY_SAME_URL_SAME_TAB
+> Automatically connects when the session was started with the same appId, in the same tab and page origin.
+
+After that, we register a function to `session.status`. This function is called by `Castify` to notify your application about any changes in ChromeCast apis. Here's the list of status that you might get
+
+| Name | Description |
+|:-|:-|
+|*Castify.SESSION_INITIALIZED*| Once session is created successfully and waits for user interaction|
+|*Castify.SESSION_STARTED*    |Once a ChromeCast device is selected from plugin list|
+|*Castify.SESSION_RECOVERED*  |Once the page is refreshed and an active session is found|
+|*Castify.SESSION_ERROR*      |Once there is a error in session. Use `session.lastError` to exam the problem|
+|*Castify.SESSION_ENDED*      |Once the session is terminated successfully|
+|*Castify.MEDIA_IDLE*         |If player is in no state|
+|*Castify.MEDIA_LOADED*       |Once the player receives a media to load|
+|*Castify.MEDIA_RECOVERED*    |Once the player is playing and browser is refreshed|
+|*Castify.MEDIA_BUFFERING*    |Once the player is buffering the stream content|
+|*Castify.MEDIA_PLAYING*      |Once the player is started to play the content|
+|*Castify.MEDIA_PAUSED*       |Once the player is successfully paused|
+|*Castify.MEDIA_SEEK_UPDATED* |Calls every second once the stream url is being played|
+|*Castify.MEDIA_STOPPED*      |Once the player is successfully stopped|
+|*Castify.MEDIA_ERROR*        |Once the player is encounter an error. `lastError` can be used to retrieve the error|
+
+So let's load a free mp4 video and play it on ChromeCast. Before doing that, we need to create media object. Media objects are created by `Castify.createMedia(options)` function. Once you created a media object you can load it by using `session.load(media)` method.
+
+> **Note** The media stream is provided by one of the google examples. I  used `Google url shortener` to make it small enough so my script looks good.
+
+Here's the full example
+
+```js
+function myApp() {
+	var media;
+	
+	//create a session
+	var session = Castify.createSession({
+		policy: Castify.POLICY_SAME_URL
+	});
+
+	session.status = function (status) {
+		Castify.log(status);
+		switch(status) {
+			//we are waiting until user select the device
+			case Castify.SESSION_STARTED:
+				//create a media object
+				media = Castify.createMedia({
+					//this is the stream url
+                    url: "http://goo.gl/oCSFGZ",
+                    type: "video/mp4",
+                    cover: {
+                        title: "Big Buck Bunny",
+                        //this is the poster url
+                        url: "http://goo.gl/E7f5KI"
+                    }
+                });	
+                //load the media object
+				session.load(media);		
+				break;
+				
+			default:
+				//do nothing
+		}
+	};
+}
+
+Castify.ready(function (ready, error) {
+	if (ready) {
+		myApp();
+	} else {
+		console.log(error);
+	}
+});
+```
+
+
+### Method References
+
+#### Castify
+
+Castify has 3 class methods
+
+1. Castify.createSession(options) which creates a session object
+2. Castify.createMedia(options) which creates a media object
+3. Castify.log(status) which prints a proper message by using `console.log`
+
+
+#### Session
+
+The following are all public available methods for `session` object.
+
+| Method name | Description |
+|:-|:-|
+|load(media)|Loads the media object. media object can be created by `Castify.createMedia`|
+|play()|It plays a paused or loaded media|
+|paused()|It pauses a playing media|
+|stop()|It stops the playing or paused media|
+|seek(value)|it sets a new value for forwarding or backwarding media content|
+|mute(enable)|It mutes a volume by passing a boolean variable|
+|end|It terminates session which cause ChromeCast to terminate the app|
+|status(status)|It needs to be set and will be called by `Castify.Session` object directly |
---
+++ castify.js
@@ -0,0 +1,439 @@
+/**
+ * Created by Ali Najafizadeh
+ * Castify.js 0.1.0
+ * https://github.com/alinz/Castify.js.git
+ *
+ * License: MIT
+ */
+
+function Castify() {}
+
+Castify.VERSION = "0.1.0";
+
+Castify.__counter = (function () {
+    var internalCount = 0;
+    return function () {
+       return internalCount++;
+    };
+}());
+
+Castify.SESSION_INITIALIZED         = Castify.__counter();
+Castify.SESSION_STARTED             = Castify.__counter();
+Castify.SESSION_RECOVERED           = Castify.__counter();
+Castify.SESSION_ERROR               = Castify.__counter();
+Castify.SESSION_ENDED               = Castify.__counter();
+
+Castify.MEDIA_IDLE                  = Castify.__counter();
+Castify.MEDIA_LOADED                = Castify.__counter();
+Castify.MEDIA_RECOVERED             = Castify.__counter();
+Castify.MEDIA_BUFFERING             = Castify.__counter();
+Castify.MEDIA_PLAYING               = Castify.__counter();
+Castify.MEDIA_PAUSED                = Castify.__counter();
+Castify.MEDIA_SEEK_UPDATED          = Castify.__counter();
+Castify.MEDIA_STOPPED               = Castify.__counter();
+Castify.MEDIA_ERROR                 = Castify.__counter();
+
+Castify.MEDIA_VOLUME_CHNAGED        = Castify.__counter();
+Castify.MEDIA_VOLUME_MUTED          = Castify.__counter();
+Castify.MEDIA_VOLUME_UNMUTED        = Castify.__counter();
+
+Castify.POLICY_SAME_URL_SAME_TAB    = Castify.__counter();
+Castify.POLICY_SAME_URL             = Castify.__counter();
+Castify.POLICY_RESTRICT             = Castify.__counter();
+
+Castify.log = function (status) {
+    switch (status) {
+        case Castify.SESSION_INITIALIZED:
+            status = "SESSION_INITIALIZED";
+            break;
+        case Castify.SESSION_STARTED:
+            status = "SESSION_STARTED";
+            break;
+        case Castify.SESSION_RECOVERED:
+            status = "SESSION_RECOVERED";
+            break;
+        case Castify.SESSION_ERROR:
+            status = "SESSION_ERROR";
+            break;
+        case Castify.SESSION_ENDED:
+            status = "SESSION_ENDED";
+            break;
+        case Castify.MEDIA_IDLE:
+            status = "MEDIA_IDLE";
+            break;
+        case Castify.MEDIA_LOADED:
+            status = "MEDIA_LOADED";
+            break;
+        case Castify.MEDIA_RECOVERED:
+            status = "MEDIA_RECOVERED";
+            break;
+        case Castify.MEDIA_BUFFERING:
+            status = "MEDIA_BUFFERING";
+            break;
+        case Castify.MEDIA_PLAYING:
+            status = "MEDIA_PLAYING";
+            break;
+        case Castify.MEDIA_PAUSED:
+            status = "MEDIA_PAUSED";
+            break;
+        case Castify.MEDIA_SEEK_UPDATED:
+            status = "MEDIA_SEEK_UPDATED";
+            break;
+        case Castify.MEDIA_STOPPED:
+            status = "MEDIA_STOPPED";
+            break;
+        case Castify.MEDIA_ERROR:
+            status = "MEDIA_ERROR";
+            break;
+        case Castify.MEDIA_VOLUME_CHNAGED:
+            status = "MEDIA_VOLUME_CHNAGED";
+            break;
+        case Castify.MEDIA_VOLUME_MUTED:
+            status = "MEDIA_VOLUME_MUTED";
+            break;
+        case Castify.MEDIA_VOLUME_UNMUTED:
+            status = "MEDIA_VOLUME_UNMUTED";
+            break;
+        default:
+            status = "UNKNOWN_STATUS (" + status + ")";
+    }
+    console.log(status);
+};
+
+Castify.ready = function (cb) {
+    var ref;
+    if (window['__onGCastApiAvailable']) {
+        ref = window['__onGCastApiAvailable'];
+    }
+
+    window['__onGCastApiAvailable'] = function(loaded, errorInfo) {
+        if (ref) {
+            ref(loaded, errorInfo);
+        }
+        cb(loaded, errorInfo);
+    };
+};
+
+Castify.Session = function (appId, policy) {
+    var that = this;
+
+    that._appId = that._parseAppId(appId);
+    that._policy = that._parsePolicy(policy);
+
+    that._chromeCastSession = null;
+    that.currentMedia = null;
+
+    that.lastStatus = -1;
+    that.lastError = null;
+
+    that._intervalTimerId = 0;
+    that.duration = { currentTime: 0, totalTime: 0 };
+
+    that._init();
+};
+
+Castify.Session.prototype = {
+    _setStatus: function (status) {
+        var that = this;
+        if ((that.lastStatus == Castify.SESSION_RECOVERED && status == Castify.SESSION_ERROR) ||
+            (status == Castify.SESSION_ERROR && that._chromeCastSession)) {
+            //ignore this error
+            that.lastStatus = -1;
+            return;
+        }
+
+        if (status == Castify.SESSION_ENDED) {
+            that.currentMedia = null;
+            that._chromeCastSession = null;
+            that._appId = null;
+            that._policy = null;
+            that.lastError = null;
+
+            clearInterval(that._intervalTimerId);
+            if (that.duration) {
+                that.duration.currentTime = 0;
+                that.duration.totalTime = 0;
+            }
+        }
+
+        that.status(status);
+        that.lastStatus = status;
+    },
+    _sessionActiveStatus: function (isAlive) {
+        if (!isAlive) {
+            this._setStatus(Castify.SESSION_ENDED);
+        }
+    },
+    _parseAppId: function (appId) {
+        if (typeof appId === "undefined") {
+            appId = chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID;
+        }
+        return appId;
+    },
+    _parsePolicy: function (policy) {
+        var that = this;
+        switch (policy) {
+            case Castify.POLICY_RESTRICT: //no auto join
+                that._policy = chrome.cast.AutoJoinPolicy.PAGE_SCOPED;
+                break;
+            case Castify.POLICY_SAME_URL_SAME_TAB: //same appID, same URL, same tab
+                that._policy = chrome.cast.AutoJoinPolicy.TAB_AND_ORIGIN_SCOPED;
+                break;
+            case Castify.POLICY_SAME_URL: //same appID and same origin URL
+                that._policy = chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED;
+                break;
+            default:
+                that._policy = chrome.cast.AutoJoinPolicy.PAGE_SCOPED;
+        }
+    },
+    _init: function () {
+        var that = this,
+            sessionRequest = new chrome.cast.SessionRequest(that._appId),
+            apiConfig = new chrome.cast.ApiConfig(
+                sessionRequest,
+                //sessionListener
+                function (chromeCastSession) {
+                    that._chromeCastSession = chromeCastSession;
+                    chromeCastSession.addUpdateListener(that._sessionActiveStatus.bind(that));
+                    that._setStatus(Castify.SESSION_RECOVERED);
+                    if (chromeCastSession.media && chromeCastSession.media.length > 0) {
+                        that.currentMedia = chromeCastSession.media[0];
+                        that._setStatus(Castify.MEDIA_RECOVERED);
+                        that._seekUpdater();
+                    }
+                },
+                //receiverListener
+                function (chromeCastEvent) {
+                    if (chromeCastEvent === chrome.cast.ReceiverAvailability.AVAILABLE) {
+                        chrome.cast.requestSession(function (chromeCastSession) {
+                            that._chromeCastSession = chromeCastSession;
+                            that._setStatus(Castify.SESSION_STARTED);
+                            chromeCastSession.addUpdateListener(that._sessionActiveStatus.bind(that));
+                        },
+                        function (err) {
+                            that.lastError = err;
+                            that._setStatus(Castify.SESSION_ERROR);
+                        });
+                    } else {
+                        //not connected.
+                    }
+                },
+                that._policy
+            );
+
+        chrome.cast.initialize(
+            apiConfig,
+            //onInitialSuccess
+            function () {
+                that._setStatus(Castify.SESSION_INITIALIZED);
+            },
+            //onInitialError
+            function (err) {
+                that.lastError = err;
+                that._setStatus(Castify.SESSION_ERROR);
+            }
+        );
+    },
+    _seekUpdater: function () {
+        var that = this;
+        that._intervalTimerId = setInterval(function () {
+            if (that.currentMedia && that.currentMedia.media && that.currentMedia.media.duration != null) {
+                var cTime = that.currentMedia.getEstimatedTime();
+                that.duration.currentTime = cTime;
+                that.duration.totalTime = that.currentMedia.media.duration;
+                that._setStatus(Castify.MEDIA_SEEK_UPDATED);
+            }
+            else {
+                if (that._intervalTimerId) {
+                    clearInterval(that._intervalTimerId);
+                }
+            }
+        }, 1000);
+    },
+    load: function (media) {
+        var that = this;
+        if (that._chromeCastSession) {
+            that._setStatus(Castify.MEDIA_BUFFERING);
+            that._chromeCastSession.loadMedia(
+                media.request,
+                function (chromeCastMedia) {
+                    that.currentMedia = chromeCastMedia;
+                    that._setStatus(Castify.MEDIA_LOADED);
+
+                    chromeCastMedia.addUpdateListener(function (value) {
+                        if (value) {
+                            that._setStatus(Castify.MEDIA_PLAYING);
+
+                            that._seekUpdater();
+                        }
+                    });
+                },
+                function (err) {
+                    that.currentMedia = null;
+                    that.lastError = err;
+                    that._setStatus(Castify.MEDIA_ERROR);
+                }
+            );
+        }
+    },
+    play: function () {
+        var that = this;
+        that.currentMedia.play(
+            null,
+            function () {
+                that._setStatus(Castify.MEDIA_PLAYING);
+                that._seekUpdater();
+            },
+            function (err) {
+                that.lastError = err;
+                that._setStatus(Castify.MEDIA_ERROR);
+            }
+        );
+    },
+    stop: function () {
+        var that = this;
+        that.currentMedia.stop(
+            null,
+            function () {
+                that._setStatus(Castify.MEDIA_STOPPED);
+            },
+            function (err) {
+                that.lastError = err;
+                that._setStatus(Castify.MEDIA_ERROR);
+            }
+        );
+    },
+    pause: function () {
+        var that = this;
+        that.currentMedia.pause(
+            null,
+            function () {
+                clearInterval(that._intervalTimerId);
+                that._setStatus(Castify.MEDIA_PAUSED);
+            },
+            function (err) {
+                that.lastError = err;
+                that._setStatus(Castify.MEDIA_ERROR);
+            }
+        );
+    },
+    seek: function (value) {
+        var that = this,
+            seekRequest = new chrome.cast.media.SeekRequest();
+
+        clearInterval(this._intervalTimerId);
+
+        seekRequest.currentTime = value * that.currentMedia.media.duration / 100;
+        currentMedia.seek(
+            seekRequest,
+            function () {
+                that._seekUpdater();
+            },
+            function (err) {
+                that.lastError = err;
+                that._setStatus(Castify.MEDIA_ERROR);
+            }
+        );
+    },
+    mute: function (enabled) {
+        chrome.cast.Volume(null, !!enabled);
+        this._setStatus(enabled? Castify.MEDIA_VOLUME_MUTED : Castify.MEDIA_VOLUME_UNMUTED);
+    },
+    end: function () {
+        var that = this;
+        if (that._chromeCastSession) {
+            that._chromeCastSession.stop(
+                //onSuccess
+                function () {
+                    //No need to call 'Castify.SESSION_ENDED' explicitly. Since _sessionActiveStatus method
+                    //will be called once the termination is done.
+                    //that._setStatus(Castify.SESSION_ENDED);
+                },
+                //onError
+                function (err) {
+                    this.lastError = err;
+                    that._setStatus(Castify.SESSION_ERROR);
+                }
+            );
+        }
+    },
+    status: function(status) {
+        Castify.log(status);
+        if (status == Castify.SESSION_ERROR) {
+            console.log(this.lastError);
+        }
+    }
+};
+
+/**
+ * create or recover session
+ * {
+ *      appId: "",
+ *      policy: ""
+ * }
+ * @param options
+ * @returns {Castify.Session}
+ */
+Castify.createSession = function (options) {
+    options = options || {};
+    return new Castify.Session(
+        options.appId,
+        options.policy
+    );
+};
+
+/**
+ *
+ * @param url [Required] is a string contains url stream to media content
+ * @param type [Required] is a string that defines type of stream. possible values are audio/mp3 video/mp4
+ * @param cover [Optional] is an object contains url and title fields
+ * @param subtitles [Optional] is and array of subtitle object. Each subtitle object contains name, language and url
+ * @param autoPlay [Optional] is a boolean value indicating whether content needs to play upon load or not.
+ * @param startTime [Optional] is an integer value indicates the start point of media
+ *
+ */
+Castify.Media = function (url, type, cover, subtitles, autoPlay, startTime) {
+    var mediaInfo = new chrome.cast.media.MediaInfo(url);
+    mediaInfo.metadata = new chrome.cast.media.GenericMediaMetadata();
+    mediaInfo.metadata.metadataType = chrome.cast.media.MetadataType.GENERIC;
+
+    if (cover) {
+        if (cover.title) {
+            mediaInfo.metadata.title = cover.title;
+        }
+        if (cover.url) {
+            mediaInfo.metadata.images = [
+                { url: cover.url }
+            ];
+        }
+    }
+
+    mediaInfo.contentType = type;
+
+    if (typeof autoPlay === "undefined") {
+        autoPlay = true;
+    }
+
+    if (typeof startTime === "undefined") {
+        startTime = 0;
+    }
+
+    var request = new chrome.cast.media.LoadRequest(mediaInfo);
+
+    request.autoplay = autoPlay;
+    request.currentTime = startTime;
+
+    this.request = request;
+};
+
+Castify.createMedia = function (options) {
+    options = options || {};
+    return new Castify.Media(
+        options.url,
+        options.type,
+        options.cover,
+        options.subtitles,
+        options.autoPlay,
+        options.startTime
+    );
+};
